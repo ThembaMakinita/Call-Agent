0538b699bb90160847acf7155c41b7872f12744a
Themba <jefrey.makinita@net1.com>
2021-08-21 11:26:42 +0200

    Basic Get Endpoints pre-done

--- callcentre/src/main/java/com/jtm/assessment/callcentre/controller/AgentController.java
+++ callcentre/src/main/java/com/jtm/assessment/callcentre/controller/AgentController.java
@@ -1,25 +1,35 @@
 package com.jtm.assessment.callcentre.controller;
 
+import com.jtm.assessment.callcentre.database.entity.Agent;
 import com.jtm.assessment.callcentre.database.entity.Team;
+import com.jtm.assessment.callcentre.database.repo.AgentRepository;
 import com.jtm.assessment.callcentre.database.repo.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AgentController {
 
     @Autowired
-    private TeamRepository teamRepository;
+    private AgentRepository agentRepository;
 
-    @GetMapping("/Basic/getTeams")
+    @GetMapping("/Basic/getAgents")
     public ResponseEntity getAllTeams(){
-        List<Team>teams = teamRepository.findAll();
-        return new ResponseEntity(teams, HttpStatus.OK);
+        List<Agent> agents = agentRepository.findAll();
+        return new ResponseEntity(agents, HttpStatus.OK);
+    }
+
+    @GetMapping("/Basic/getAgent/{TeamId}")
+    public ResponseEntity getAllTeamById(@PathVariable(value ="TeamId") Long id){
+        Optional<Agent> agent = agentRepository.findById(id);
+        return new ResponseEntity(agent, HttpStatus.OK);
     }
 
 }
--- callcentre/src/main/java/com/jtm/assessment/callcentre/controller/TeamController.java
+++ callcentre/src/main/java/com/jtm/assessment/callcentre/controller/TeamController.java
@@ -1,7 +1,32 @@
 package com.jtm.assessment.callcentre.controller;
 
+import com.jtm.assessment.callcentre.database.entity.Team;
+import com.jtm.assessment.callcentre.database.repo.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TeamController {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @GetMapping("/Basic/getTeams")
+    public ResponseEntity getAllTeams(){
+        List<Team> teams = teamRepository.findAll();
+        return new ResponseEntity(teams, HttpStatus.OK);
+    }
+
+    @GetMapping("/Basic/getTeam/{TeamId}")
+    public ResponseEntity getAllTeamById(@PathVariable(value ="TeamId") Long id){
+        Optional<Team> teams = teamRepository.findById(id);
+        return new ResponseEntity(teams, HttpStatus.OK);
+    }
 }
